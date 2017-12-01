5dee34935289cdb577e9b7907e9e549a05b2c208
tedevicetest <32878395+tedevicetest@users.noreply.github.com>
2017-12-01 11:16:03 +0000

    Delete random_loop.rb

--- RubymineProjects/launchschool/random_loop.rb
+++
@@ -1,9 +0,0 @@
-	numbers = []
-
-		while numbers.size < 10 # sets the number of loops
-
-			numbers << rand(1000) # sets the limit of the data set here 1 - 1000
-
-	end
-
-	puts numbers # puts a random set of 10 numbers between 1-1000
