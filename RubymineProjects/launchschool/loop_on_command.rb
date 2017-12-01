3530ab39da2763c616914e34f9677c24936e4996
tedevicetest <32878395+tedevicetest@users.noreply.github.com>
2017-12-01 11:22:32 +0000

    Delete loop_on_command.rb

--- RubymineProjects/launchschool/loop_on_command.rb
+++
@@ -1,7 +0,0 @@
-loop do
-	puts 'Should I stop looping?'
-	answer = gets.chomp
-	answer.downcase! # This line used so however Yes is typed code only have to check for 'yes'
-	break if answer == 'yes'
-	puts 'Incorrect answer. Please answer "yes".'
-end
